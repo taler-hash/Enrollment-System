cec58c6a596bab25e31ead523ee58e04fd3372c5
Jurie Tylier Pedrogas <juriepedrogas@gmail.com>
2025-03-25 11:25:38 +0800

    ES-02: added student migration

---
+++ database/migrations/2025_03_25_032247_create_students_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('birth_date');
+            $table->string('student_id');
+            $table->string('parent_name');
+            $table->string('parent_contact_number');
+            $table->string('email_address');
+            $table->string('parent_relationship');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
